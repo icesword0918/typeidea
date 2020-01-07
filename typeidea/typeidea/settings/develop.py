e6477704ca299ba6a4a225a725a9cbb7706c0402
git <152119156@qq.com>
2020-01-07 17:32:06 +0800

    拆分settings适应不同环境与配置settings

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,21 @@
+from.base import *  # NOQA
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        """
+        # 配置sqlite3数据库
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        """
+        # 配置mysql数据库
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'Admin_0820!',
+        'HOST': '152.136.194.98',
+        'PORT': '3306',
+
+    }
+}
