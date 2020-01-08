91a2d73243bc43a6c7d4e767c85832b85f27994e
wzg <152119156@qq.com>
2020-01-08 22:14:29 +0800

    编写model层代码，并创建数据表

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -1,17 +1,19 @@
 from.base import *  # NOQA
+import pymysql
+pymysql.install_as_MySQLdb()
 
 DEBUG = True
 
 DATABASES = {
     'default': {
-        """
-        # 配置sqlite3数据库
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        """
+
+        # # 配置sqlite3数据库
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
         # 配置mysql数据库
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
+        'NAME': 'blog',
         'USER': 'root',
         'PASSWORD': 'Admin_0820!',
         'HOST': '152.136.194.98',
