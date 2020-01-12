6ea5c2136ba4d82d5c80b6b0acaee0c3ec99218c
git <152119156@qq.com>
2020-01-12 23:40:29 +0800

    自定义侧边栏与自定义文章列表

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -22,6 +22,20 @@ class CategoryAdmin(admin.ModelAdmin):
     post_count.short_description = '文章数量'
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤只展示当前用户分类"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id','name')
+
+    def queryset(self, request, queryset):
+        category_id=self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
 # 标签
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -37,7 +51,7 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
-        'title', 'category', 'status', 'created_time', 'operator'
+        'title', 'category', 'status','owner', 'created_time', 'operator'
     ]
     list_display_links = []
 
@@ -69,3 +83,7 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin,self).get_queryset(request)
+        return  qs.filter(owner=request.user)
